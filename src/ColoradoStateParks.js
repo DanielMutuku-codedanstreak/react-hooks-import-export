efa7acdbb70e2d1835deaa4d76a04b3ff6a0a09f
Daniel Mutuku <danielmakau507@gmail.com>
2023-10-27 12:29:18 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,20 +1,10 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-import MesaVerde from "./parks/MesaVerde";
-import {trees, wildlife, elevation} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return (
-    <div>
-      <h1>Colorado State Parks!</h1>
-      <MesaVerde></MesaVerde>
-      <p>{trees}</p>
-      <p>{wildlife}</p>
-      <p>{elevation}</p>
-    </div>
-  )
+  return <h1>Colorado State Parks!</h1>;
 }
 
 export default ColoradoStateParks
